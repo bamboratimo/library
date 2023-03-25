aa9ba4a5849debf655acbaa8db2d96ba9e19cee9
Timo Rautiainen <timo.rautiainen94@gmail.com>
2023-03-25 11:28:59 +0200

    setup files

---
+++ main.js
@@ -0,0 +1,9 @@
+function Book(title, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return title + " by " + author + ", " + pages + " pages, " + read;
+  };
+}
