1a26a16bf2d250188a6975e3aec299083bc00fb1
Timo Rautiainen <timo.rautiainen94@gmail.com>
2023-03-27 17:19:42 +0300

    add function for looping through books and make 2 hard coded books

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
 let myLibrary = [
-  {
-    title: "kakka",
-    author: "minä",
-    pages: 20,
-    read: true,
-  },
+    {
+        title: "Taru sormusten herrasta",
+        author: "J.R.R. Tolkien",
+        pages: 2000,
+        read: true,
+    },
+    {
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+        pages: 1000,
+        read: false,
+    },
 ];
 
+console.log(myLibrary[0].title);
+
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function () {
-    return title + " by " + author + ", " + pages + " pages, " + read;
-  };
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return title + " by " + author + ", " + pages + " pages, " + read;
+    };
 }
 
 function addBookToLibrary(newBook) {
-  myLibrary.push(newBook);
+    myLibrary.push(newBook);
 }
 
-function loopMyLibrary() {}
+function loopMyLibrary() {
+    let booksContainer = document.querySelector(".books-cont");
+    myLibrary.forEach((book) => {
+        let div = document.createElement("div");
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        let p3 = document.createElement("p");
+        let p4 = document.createElement("p");
+        p1.textContent = book.title;
+        p2.textContent = book.author;
+        p3.textContent = book.pages;
+        p4.textContent = book.read;
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        div.appendChild(p4);
+        div.classList.add("book-card");
+        booksContainer.appendChild(div);
+    });
+}
+
+loopMyLibrary();
