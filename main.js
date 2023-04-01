372f26ead3680e4c9dbd62eb211827304ed7a023
Timo Rautiainen <timo.rautiainen94@gmail.com>
2023-04-02 01:51:03 +0300

    add delete button in cards and write logic for it

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-let myLibrary = [
-    {
-        title: "Taru sormusten herrasta",
-        author: "J.R.R. Tolkien",
-        pages: 2000,
-        read: true,
-    },
-    {
-        title: "Harry Potter",
-        author: "J.K. Rowling",
-        pages: 1000,
-        read: false,
-    },
-];
+let myLibrary = [];
 
 let form = document.querySelector(".book-form");
-form.style.visibility = "hidden";
+form.addEventListener("submit", addBookToLibrary);
+
+let newBook = document.querySelector(".new-book-btn");
+newBook.addEventListener("click", displayForm);
+
+let submitBtn = document.querySelector(".submit-btn");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -23,36 +15,57 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
+function addBookToLibrary(e) {
+    e.preventDefault();
+    form.style.visibility = "hidden";
+    let title = document.querySelector(".title").value;
+    let author = document.querySelector(".author").value;
+    let pages = document.querySelector(".pages").value;
+    myLibrary.push(new Book(title, author, pages));
+    loopMyLibrary();
 }
 
 function loopMyLibrary() {
+    let cards = document.querySelectorAll(".book-card");
+    cards.forEach((card) => {
+        card.remove();
+    });
+    createCard();
+}
+
+function deleteCard(e) {
+    let element = e.target.parentElement;
+    let index = element.dataset.indeksi;
+    myLibrary.splice(index, 1);
+    loopMyLibrary();
+}
+
+function createCard() {
     let booksContainer = document.querySelector(".books-cont");
-    myLibrary.forEach((book) => {
+    myLibrary.forEach((book, index) => {
         let div = document.createElement("div");
+        div.dataset.indeksi = index;
+        let deleteBtn = document.createElement("div");
+        deleteBtn.textContent = "X";
+        deleteBtn.classList.add("delete-btn");
         for (let key in book) {
             let p = document.createElement("p");
             p.textContent = key + ": " + book[key];
             div.appendChild(p);
-            div.classList.add("book-card");
-            booksContainer.appendChild(div);
         }
+        div.classList.add("book-card");
+        booksContainer.appendChild(div);
+        div.appendChild(deleteBtn);
+        deleteBtn.addEventListener("click", deleteCard);
     });
 }
-addBookToLibrary(new Book("Arikka56", "Helitys", 10, true));
-loopMyLibrary();
-
-let newBook = document.querySelector(".new-book-btn");
-let submitBtn = document.querySelector(".submit-btn");
-
-function submitForm(e) {
-    e.preventDefault();
-    form.style.visibility = "hidden";
-}
-form.addEventListener("submit", submitForm);
 
 function displayForm() {
+    let fields = document.querySelectorAll(".input-field");
+    fields.forEach((field) => {
+        field.value = "";
+    });
     form.style.visibility = "visible";
+    let radioYes = document.querySelector(".radio-yes");
+    radioYes.checked = true;
 }
-newBook.addEventListener("click", displayForm);
